f6622ab54c15a2999b71528172cae88c2147099b
Precious Mamotingoe Lesupi <preciousmlesupi@gmail.com>
2024-01-01 02:28:20 +0200

    time functions

---
+++ ReminderApp/main.py
@@ -0,0 +1,7 @@
+import time
+# what to remind about
+remind = input("what should I remind you about?")
+# number of minutes
+minutes = float(input("In how many minutes?"))
+# convert to seconds
+local_time = minutes * 60
