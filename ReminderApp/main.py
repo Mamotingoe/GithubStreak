0fd3b396019d6d237ee72695ae8f4aa4dc9e7c44
Precious Mamotingoe Lesupi <preciousmlesupi@gmail.com>
2024-01-02 05:56:10 +0200

    program pause and fin

--- ReminderApp/main.py
+++ ReminderApp/main.py
@@ -4,4 +4,7 @@ remind = input("what should I remind you about?")
 # number of minutes
 minutes = float(input("In how many minutes?"))
 # convert to seconds
-local_time = minutes * 60
+time_sec = minutes * 60
+#pause the program
+time.sleep(time_sec)
+print(remind)
