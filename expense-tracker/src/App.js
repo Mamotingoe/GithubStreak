5962467ad1dccc98e02b92b234de01ad9fe3ca0b
Precious Mamotingoe Lesupi <preciousmlesupi@gmail.com>
2024-01-10 04:43:42 +0200

    Income and Expenses box

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { IncomeExpenses } from './components/IncomeExpenses';
+
 import './App.css';
 function App() {
   return (
@@ -7,6 +10,7 @@ function App() {
       <Header />
       <div className="container">
         <Balance />
+        <IncomeExpenses />
       </div>
 
     </div>
--- expense-tracker/src/components/IncomeExpenses.js
+++ expense-tracker/src/components/IncomeExpenses.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 export const IncomeExpenses = () => {
   return (
-    <div>
+    <div class="inc-exp-container">
+      <div>
+        <h4>Income</h4>
+        <p id="money-plus" class="money-plus">+R0.00</p>
+      </div>
+      <div>
+        <h4>Expense</h4>
+        <p id="money-minus" class="money-minus">-R0.00</p>
+      </div>
 
     </div>
   )
