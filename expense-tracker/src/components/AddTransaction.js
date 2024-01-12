bf05ff1d6363ce6a41f53e55bb586625bc0c524c
Precious Mamotingoe Lesupi <preciousmlesupi@gmail.com>
2024-01-12 04:05:23 +0200

    transaction form

---
+++ expense-tracker/src/components/AddTransaction.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const AddTransaction = () => {
+  return (
+    <div>
+      <h3>Add new transaction</h3>
+      <form id="form">
+        <div class="form-control">
+          <label for="text">Text</label>
+        </div>
+      </form>
+
+    </div>
+  )
+}
