ab33fb9be5488d3b444fec1569d02bd5f3d72a31
Precious Mamotingoe Lesupi <preciousmlesupi@gmail.com>
2024-01-13 06:57:19 +0200

    debug add transaction

--- expense-tracker/src/components/AddTransaction.js
+++ expense-tracker/src/components/AddTransaction.js
@@ -3,21 +3,21 @@ import React from 'react'
 export const AddTransaction = () => {
   return (
     <>
-      <h3>Add new transaction</h3>
-      <form>
-        <div class="form-control">
-          <label for="text">Text</label>
-          <input type="text" placeholder="Enter text..."/>
-        </div>
-        <div class="form-control">
-          <label htmlFor="amount"
+    <h3>Add new transaction</h3>
+    <form>
+      <div className="form-control">
+        <label htmlFor="text">Text</label>
+        <input type="text" placeholder="Enter text..." />
+      </div>
+      <div className="form-control">
+        <label htmlFor="amount"
           >Amount <br />
           (negative - expense, positive - income)</label
-          >
-          <input type="number" placeholder = "Enter amount..."/>
-          <button className="btn">Add transaction</button>
-      </form>
-
-    <>
+        >
+        <input type="number" placeholder="Enter amount..." />
+      </div>
+      <button className="btn">Add transaction</button>
+    </form>
+  </>
   )
 }
