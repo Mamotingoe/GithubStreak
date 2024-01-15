c5708c945b029ba4973c235a9906f2ac515188d1
Precious Mamotingoe Lesupi <preciousmlesupi@gmail.com>
2024-01-15 05:29:15 +0200

    transaction list update

--- expense-tracker/src/components/TransactionList.js
+++ expense-tracker/src/components/TransactionList.js
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { Transaction } from './Transaction';
+
+import { GlobalContext } from '../context/GlobalState';
 
 export const TransactionList = () => {
+  const { transactions } = useContext(GlobalContext);
+
   return (
     <>
       <h3>History</h3>
-      <ul id="list" class="list">
-        <li className="minus">
-          Cash <span>-R400</span><button className="delete-btn">x</button>
-        </li>
+      <ul className="list">
+        {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
       </ul>
-
-    <>
+    </>
   )
 }
